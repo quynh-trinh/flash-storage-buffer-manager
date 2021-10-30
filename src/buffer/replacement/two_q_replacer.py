6e376e1c32828b341420993fdd158ba53f4e192c
qtrinh6 <qtrinh6@gatech.edu>
2021-10-30 13:54:36 -0400

    update two_q but need to update the tests

--- src/buffer/replacement/two_q_replacer.py
+++ src/buffer/replacement/two_q_replacer.py
@@ -1,14 +1,15 @@
 from threading import Lock
 from src.buffer.replacement.abstract_replacer import AbstractReplacer
+from src.util.constants import INVALID_PAGE_ID
 
 
 class TwoQReplacer(AbstractReplacer):
-    def __init__(self, frame_count):
-        super().__init__(frame_count)
+    def __init__(self, page_count):
+        super().__init__(page_count)
         self._mutex = Lock()
         self._fifo_queue = []
         self._lru_queue = []
-        self._frames = {} # key=frame_id; value=[<in_fifo>, <isPinned>]
+        self._pages = {} # key=page_id; value=[<in_fifo>, <isPinned>]
 
     @property
     def fifo_q(self):
@@ -20,35 +21,41 @@ class TwoQReplacer(AbstractReplacer):
 
     def pin_page(self, frame_id: int):
         self._mutex.acquire()
-        if frame_id not in self._frames: # frame has not been used
-            self._frames[frame_id] = [True, True]
+        if frame_id not in self._pages: # page has not been used
+            self._pages[frame_id] = [True, True]
             self._fifo_queue.append(frame_id)
         else:
-            if self._frames[frame_id][0] is True: # frame is in fifo
+            if self._pages[frame_id][0]: # page is in fifo
                 self._fifo_queue.remove(frame_id)
-                self._lru_queue.append(frame_id)
-            self._frames[frame_id] = [False, True] # frame is in lru and is being pinned
+            else:
+                self._lru_queue.remove(frame_id)
+            self._lru_queue.append(frame_id)
+            self._pages[frame_id] = [False, True] # page is in lru and is being pinned
         self._mutex.release()
 
-    def unpin_page(self, frame_id: int):
+    def unpin_page(self, page_id: int, dirty: bool):
         self._mutex.acquire()
-        if frame_id not in self._frames:
+        if page_id not in self._pages:
             self._mutex.release()
-            raise ValueError(f"Error: Frame with ID {frame_id} is not found. Unpin could not be done.")
-        self._frames[frame_id][1] = False
+            raise ValueError(f"Error: Page {page_id} is not found. Failed to unpin.")
+        self._pages[page_id][1] = False
         self._mutex.release()
 
     def get_victim(self) -> int:
         self._mutex.acquire()
         victim = -1
-        for i, f in enumerate(self._fifo_queue):
-            if self._frames[f][1] is False:
-                victim = f
+        for p in self._fifo_queue:
+            if self._pages[p][1] is False:
+                victim = p
+                self._fifo_queue.remove(p)
                 break
         if victim < 0:
-            for i, f in enumerate(self._lru_queue):
-                if self._frames[f][1] is False:
-                    victim = f
+            for p in self._lru_queue:
+                if self._pages[p][1] is False:
+                    victim = p
+                    self._lru_queue.remove(p)
                     break
+        if victim != INVALID_PAGE_ID:
+            self._pages.pop(victim)
         self._mutex.release()
         return victim
